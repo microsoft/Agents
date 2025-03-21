a46ec409e94a9f5fec963d7fc8a88e23f40ec6ab
rido <rmpablos@microsoft.com>
2025-03-21 16:58:42 -0700

    Add WeatherBot sample with Node.js implementation and necessary configurations

---
+++ samples/basic/weatherbot/nodejs/src/index.ts
@@ -0,0 +1,21 @@
+import { AuthConfiguration, authorizeJWT, CloudAdapter, loadAuthConfigFromEnv, Request } from '@microsoft/agents-bot-hosting'
+import express, { Response } from 'express'
+import { WeatherAgent } from './myAgent.js'
+const authConfig: AuthConfiguration = loadAuthConfigFromEnv()
+const adapter = new CloudAdapter(authConfig)
+
+const server = express()
+server.use(express.json())
+server.use(authorizeJWT(authConfig))
+
+
+server.post('/api/messages', async (req: Request, res: Response) => {
+    await adapter.process(req, res, async (context) => {
+      await WeatherAgent.run(context)
+    })
+  })
+  
+  const port = process.env.PORT || 3978
+  server.listen(port, () => {
+    console.log(`\nServer listening to port ${port} for appId ${authConfig.clientId} debug ${process.env.DEBUG}`)
+  })
