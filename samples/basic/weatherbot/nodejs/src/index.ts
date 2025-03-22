235011e4abc4343200f31f3df5d5393818e8a2ca
rido <rmpablos@microsoft.com>
2025-03-21 17:13:17 -0700

    Refactor WeatherBot code for improved readability and consistency

--- samples/basic/weatherbot/nodejs/src/index.ts
+++ samples/basic/weatherbot/nodejs/src/index.ts
@@ -8,14 +8,13 @@ const server = express()
 server.use(express.json())
 server.use(authorizeJWT(authConfig))
 
-
 server.post('/api/messages', async (req: Request, res: Response) => {
-    await adapter.process(req, res, async (context) => {
-      await WeatherAgent.run(context)
-    })
+  await adapter.process(req, res, async (context) => {
+    await WeatherAgent.run(context)
   })
-  
-  const port = process.env.PORT || 3978
-  server.listen(port, () => {
-    console.log(`\nServer listening to port ${port} for appId ${authConfig.clientId} debug ${process.env.DEBUG}`)
-  })
+})
+
+const port = process.env.PORT || 3978
+server.listen(port, () => {
+  console.log(`\nServer listening to port ${port} for appId ${authConfig.clientId} debug ${process.env.DEBUG}`)
+})
--- samples/basic/weatherbot/nodejs/src/myAgent.ts
+++ samples/basic/weatherbot/nodejs/src/myAgent.ts
@@ -1,28 +1,28 @@
-import { ActivityTypes, ApplicationBuilder, CardFactory, MessageFactory } from '@microsoft/agents-bot-hosting'
-import { AzureChatOpenAI } from "@langchain/openai";
-import { MemorySaver } from "@langchain/langgraph";
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
-import { createReactAgent } from "@langchain/langgraph/prebuilt";
-import { GetWeatherTool } from "./tools/getWeatherTool.js";
-import { dateTool } from "./tools/dateTimeTool.js";
+import { ActivityTypes, ApplicationBuilder, MessageFactory } from '@microsoft/agents-bot-hosting'
+import { AzureChatOpenAI } from '@langchain/openai'
+import { MemorySaver } from '@langchain/langgraph'
+import { HumanMessage, SystemMessage } from '@langchain/core/messages'
+import { createReactAgent } from '@langchain/langgraph/prebuilt'
+import { GetWeatherTool } from './tools/getWeatherTool.js'
+import { dateTool } from './tools/dateTimeTool.js'
 
 export const WeatherAgent = new ApplicationBuilder().build()
 
-const agentTools = [GetWeatherTool, dateTool];
+const agentTools = [GetWeatherTool, dateTool]
 const agentModel = new AzureChatOpenAI({
-  azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY, 
-  azureOpenAIApiInstanceName:  process.env.AZURE_OPENAI_API_INSTANCE_NAME, 
+  azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
+  azureOpenAIApiInstanceName: process.env.AZURE_OPENAI_API_INSTANCE_NAME,
   azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME,
-  azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION, 
+  azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION,
   temperature: 0
-});
+})
 
-const agentCheckpointer = new MemorySaver();
+const agentCheckpointer = new MemorySaver()
 const agent = createReactAgent({
   llm: agentModel,
   tools: agentTools,
   checkpointSaver: agentCheckpointer,
-});
+})
 
 // const sysMessage = new SystemMessage(`
 //         You are a friendly assistant that helps people find a weather forecast for a given time and place.
@@ -30,7 +30,7 @@ const agent = createReactAgent({
 //         but once you have a forecast forecast, make sure to format it nicely using an adaptive card.
 
 //         Respond in JSON format with the following JSON schema:
-        
+
 //         {
 //             "contentType": "'Text' or 'AdaptiveCard' only",
 //             "content": "{The content of the response, may be plain text, or JSON based adaptive card}"
@@ -40,26 +40,23 @@ const agent = createReactAgent({
 const sysMessage = new SystemMessage(`
         You are a friendly assistant that helps people find a weather forecast for a given time and place.
         You may ask follow up questions until you have enough informatioon to answer the customers question.`
-    )
+)
 
 WeatherAgent.activity(ActivityTypes.Message, async (context, state) => {
-    const agentFinalState = await agent.invoke({ 
-        messages: [
-            sysMessage,
-            new HumanMessage(context.activity.text!)
-        ]
-    },
-    { 
-        configurable: { thread_id: "42" } 
-    })
-    
-    const response = agentFinalState.messages[agentFinalState.messages.length - 1].content
-    console.log(response)
-    await context.sendTraceActivity("Agent Response", response)
-    //@ts-ignore
-    //await context.sendActivity(MessageFactory.attachment(CardFactory.adaptiveCard(response.content)))
+  // const chatHistory = state.getValue('conversation.chatHistory', () => [])
+  const agentFinalState = await agent.invoke({
+    messages: [
+      sysMessage,
+      new HumanMessage(context.activity.text!)
+    ]
+  },
+  { configurable: { thread_id: '42' } })
 
-    await context.sendActivity(MessageFactory.text(response))
+  const response = agentFinalState.messages[agentFinalState.messages.length - 1].content
+  console.log(response)
+  await context.sendTraceActivity('Agent Response', response)
+  // @ts-ignore
+  // await context.sendActivity(MessageFactory.attachment(CardFactory.adaptiveCard(response.content)))
+
+  await context.sendActivity(MessageFactory.text(response))
 })
-
-
