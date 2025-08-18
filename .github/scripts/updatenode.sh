dde5d7068d1602254d03d46efda0b09d44733bd6
Rido <rido-min@users.noreply.github.com>
2025-08-18 16:31:55 +0000

    Update log message in updatenode.sh for clarity

--- .github/scripts/updatenode.sh
+++ .github/scripts/updatenode.sh
@@ -3,7 +3,7 @@ set -e
 
 for dir in samples/nodejs/*/; do
     if [ -d "$dir" ]; then
-        echo "Running npm install in $dir"
+        echo "Updating $dir"
         (cd "$dir" && rm -rf node_modules/ package-lock.json && npm install)
     fi
 done
